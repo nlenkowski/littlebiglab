7e90302a4c21bfb2644fc5c213c7e16048525b07
Nathan Lenkowski <nathan@littlebiglab.com>
2016-07-18 22:55:17 +0200

    Add home page template and styles

--- templates/home.php
+++ templates/home.php
@@ -10,7 +10,34 @@ Template Name: Home
 
     <?php while ( have_posts() ) : the_post(); ?>
 
-        <?php get_template_part( 'partials/content', 'page' ); ?>
+        <h1 class="site-title"><?php the_field('main_heading'); ?></h1>
+
+        <section class="projects">
+            <h2 class="section-title">
+                <?php the_field('projects_heading'); ?>
+            </h2>
+        </section>
+
+        <?php if( have_rows('sections') ): ?>
+
+        	<?php while( have_rows('sections') ): the_row();
+        		$name    = get_sub_field('name');
+        		$heading = get_sub_field('heading');
+        		$content = get_sub_field('content');
+        		?>
+
+        		<section class="<?php echo strtolower($name); ?>">
+                    <h2 class="section-title">
+                        <?php echo $heading; ?>
+                    </h2>
+                    <div class="section-content">
+                        <?php echo $content; ?>
+                    </div>
+        		</section>
+
+        	<?php endwhile; ?>
+
+        <?php endif; ?>
 
     <?php endwhile; ?>
 
