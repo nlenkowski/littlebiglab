6e1c719f0becc746aa9e94f04ea8e19232ba3180
Nathan Lenkowski <nathan@littlebiglab.com>
2016-08-11 07:55:08 +0200

    Improve indentation and spacing

--- templates/home.php
+++ templates/home.php
@@ -10,36 +10,37 @@ Template Name: Home
 
     <?php while ( have_posts() ) : the_post(); ?>
 
-        <h1 class="site-title"><?php the_field('main_heading'); ?></h1>
-
         <section class="projects">
+
             <h2 class="section-title">
                 <?php the_field('projects_heading'); ?>
             </h2>
+
+            <?php if( have_rows('sections') ): ?>
+
+            	<?php while( have_rows('sections') ): the_row();
+            		$name    = get_sub_field('name');
+            		$heading = get_sub_field('heading');
+            		$content = get_sub_field('content');
+            		?>
+
+            		<section class="<?php echo strtolower($name); ?>">
+
+                        <h2 class="section-title">
+                            <?php echo $heading; ?>
+                        </h2>
+
+                        <div class="section-content">
+                            <?php echo $content; ?>
+                        </div>
+
+            		</section>
+
+            	<?php endwhile; ?>
+            <?php endif; ?>
         </section>
 
-        <?php if( have_rows('sections') ): ?>
-
-        	<?php while( have_rows('sections') ): the_row();
-        		$name    = get_sub_field('name');
-        		$heading = get_sub_field('heading');
-        		$content = get_sub_field('content');
-        		?>
-
-        		<section class="<?php echo strtolower($name); ?>">
-                    <h2 class="section-title">
-                        <?php echo $heading; ?>
-                    </h2>
-                    <div class="section-content">
-                        <?php echo $content; ?>
-                    </div>
-        		</section>
-
-        	<?php endwhile; ?>
-
-        <?php endif; ?>
-
-    <?php endwhile; ?>
+    <?php endwhile; // end of the loop ?>
 
 </main>
 
