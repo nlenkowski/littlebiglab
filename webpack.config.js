6c9ed1c906b61081c2f1a6fa67e3a6f0ce5ea66c
Nathan Lenkowski <nathan@littlebiglab.com>
2017-11-21 22:17:24 +0100

    Add base webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,50 @@
 const path = require('path');
-const webpack = require("webpack");
+const webpack = require('webpack');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
 // Environment
-const nodeEnv = process.env.NODE_ENV || "production";
+const nodeEnv = process.env.NODE_ENV || 'production';
 
 // Paths
 const sourcePath = path.join(__dirname, 'assets/scripts');
 const buildPath = path.join(__dirname, 'dist-webpack');
 
 module.exports = {
-  devtool: "source-map",
+  devtool: 'source-map',
   entry: {
-    home: ["babel-polyfill", path.resolve(sourcePath, 'home.js')],
-    project: ["babel-polyfill", path.resolve(sourcePath, 'project.js')]
+    home: ['babel-polyfill', 'svgxuse', path.resolve(sourcePath, 'home.js')],
+    project: ['babel-polyfill', 'svgxuse', path.resolve(sourcePath, 'project.js')],
   },
   output: {
-    path: path.resolve(buildPath, "scripts"),
-    filename: "[name].min.js"
+    path: path.resolve(buildPath, 'scripts'),
+    filename: '[name].min.js',
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
+        loader: 'babel-loader',
+        options: {
           presets: [
-            ["env",
-              { targets: { browsers: ["last 2 versions"] } }
-            ]
-          ]
-        }
-      }
-    ]
+            ['env', { targets: { browsers: ['last 2 versions'] } }],
+          ],
+        },
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": { NODE_ENV: JSON.stringify(nodeEnv) }
-    }),
-    new UglifyJsPlugin({
-      compress: { warnings: false },
-      output: { comments: false },
-      sourceMap: true
+      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) },
     }),
     new CommonsChunkPlugin({
-      name: "common",
-      chunks: ["home", "project"],
-      minChunks: 2
+      name: 'common',
+      chunks: ['home', 'project'],
+      minChunks: 2,
     }),
     new BrowserSyncPlugin({
-      proxy: 'https://logan.local'
-    })
-  ]
+      proxy: 'https://logan.local',
+    }),
+  ],
 };
