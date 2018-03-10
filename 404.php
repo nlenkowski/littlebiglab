a2e007033556d72fac79a3fe883b41431e97ae62
Nathan Lenkowski <nathan@littlebiglab.com>
2018-03-10 15:14:54 +0100

    Customize 404 page

--- 404.php
+++ 404.php
@@ -7,10 +7,8 @@
 <?php get_header(); ?>
 
 <main class="main">
-
-    <header class="page-header">
-        <h1 class="page-title"><?php _e( 'Sorry, but the page you were trying to view does not exist.', 'blujay' ); ?></h1>
-    </header>
+    <h1 class="site-title container"><?php _e( '404. Dang.', 'blujay' ); ?></h1>
+</article>
 
 </main>
 
