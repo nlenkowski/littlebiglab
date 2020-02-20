35cf2f96ec47f19c7f81d9a7f10b215eadc05c8b
Nathan Lenkowski <nathan@littlebiglab.com>
2020-02-20 11:15:41 +0100

    Change projects page robots meta tag from nofollow to noindex

--- lib/custom.php
+++ lib/custom.php
@@ -3,15 +3,11 @@
  * Custom functions specific to this theme
  */
 
-
-/**
- * Add robots meta tag for project pages
- */
-add_action('wp_head', 'add_robots_meta_for_projects');
-function add_robots_meta_for_projects()
+// Add noindex meta tag to project pages only
+function lbl_add_noindex_meta_for_projects()
 {
     if (is_singular('project')) {
-        $meta='<meta name="robots" content="nofollow" />';
-        echo $meta;
+        echo '<meta name="robots" content="noindex" />';
     }
 }
+add_action('wp_head', 'lbl_add_noindex_meta_for_projects');
