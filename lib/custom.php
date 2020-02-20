bfad4ea2a38f3e71e5ea2cd033b912f343f8d329
Nathan Lenkowski <nathan@littlebiglab.com>
2020-02-20 11:13:58 +0100

    Remove unnecessary custom admin styles

--- lib/custom.php
+++ lib/custom.php
@@ -3,19 +3,6 @@
  * Custom functions specific to this theme
  */
 
-/**
- * Custom admin styles
- */
-function littlebiglab_custom_admin_styles()
-{
-    echo '
-    <style>
-        .cpac-column-value-image img {
-            margin: 0;
-        }
-    </style>';
-}
-add_action('admin_head', 'littlebiglab_custom_admin_styles');
 
 /**
  * Add robots meta tag for project pages
