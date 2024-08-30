65ac0d3125208dac1d47ea25fd95fe3f42a668a4
Nathan Lenkowski <nathan@littlebiglab.com>
2024-08-30 20:35:35 +0200

    Update laravel mix config to support most recent version

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,28 +1,15 @@
 /* eslint-disable no-undef */
 const mix = require("laravel-mix");
-require("@ayctor/laravel-mix-svg-sprite");
 
 mix
-  .setPublicPath(path.resolve("./"))
-  .js("./assets/scripts/home.js", "./dist/scripts")
-  .js("./assets/scripts/page.js", "./dist/scripts")
-  .js("./assets/scripts/project.js", "./dist/scripts")
-  .sass("./assets/styles/main.scss", "./dist/styles")
-  .copyDirectory("./assets/fonts", "./dist/fonts")
-  .copyDirectory("./assets/images", "./dist/images")
-  .svgSprite("assets/icons/*.svg", {
-    output: {
-      filename: "dist/icons/symbols.svg"
-    },
-    sprite: {
-      prefix: "icon-"
-    }
-  })
+  .js("assets/scripts/home.js", "dist/scripts")
+  .js("assets/scripts/page.js", "dist/scripts")
+  .js("assets/scripts/project.js", "dist/scripts")
+  .sass("assets/styles/main.scss", "dist/styles")
+  .copyDirectory("assets/fonts", "dist/fonts")
+  .copyDirectory("assets/images", "dist/images")
   .options({
     processCssUrls: false,
-    terser: {
-      extractComments: false
-    }
   })
   .extract();
 
@@ -35,7 +22,7 @@ if (!mix.inProduction()) {
     notify: false,
     files: ["dist/styles/**/*.css", "dist/scripts/**/*.js", "**/*.php"],
     snippetOptions: {
-      ignorePaths: ["admin/**"]
-    }
+      ignorePaths: ["admin/**"],
+    },
   });
 }
